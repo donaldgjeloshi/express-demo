099dafc73793571eb1e300f0b0e52f3f23ffc856
Donald Gjeloshi <dongje11@htl-shkoder.com>
2020-01-21 18:24:47 +0100

    Handling HTTP PUT Requests

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const Joi = require("joi");
 const express = require("express");
 const app = express();
 
@@ -26,6 +27,13 @@ app.get("/api/courses/:id", (req, res) => {
 });
 
 app.post("/api/courses", (req, res) => {
+  const { error } = validateCourse(req.body); // result.error
+  if (error) {
+    // 400 Bad Request
+    res.status(400).send(error.details[0].message);
+    return;
+  }
+
   const course = {
     id: courses.length + 1,
     name: req.body.name
@@ -33,6 +41,35 @@ app.post("/api/courses", (req, res) => {
   courses.push(course);
   res.send(course);
 });
+
+app.put("/api/courses/:id", (req, res) => {
+  //lookup the course
+  //if not exist -> 404
+  const course = courses.find(c => c.id === parseInt(req.params.id));
+  if (!course) {
+    res.status(404).send("The course with the given ID was not found!"); // 404
+  }
+
+  const { error } = validateCourse(req.body); // result.error
+  if (error) {
+    // 400 Bad Request
+    res.status(400).send(error.details[0].message);
+    return;
+  }
+  //update the course
+  course.name = req.body.name;
+  res.send(course);
+  // return the updated course
+});
+
+function validateCourse(course) {
+  const schema = {
+    name: Joi.string()
+      .min(3)
+      .required()
+  };
+  return Joi.validate(course, schema);
+}
 
 // PORT
 const port = process.env.PORT || 3000;
